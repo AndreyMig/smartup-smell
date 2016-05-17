f9b09311631ddb0f47b010041bf6f7730d4563cb
andreymig <migalovich@gmail.com>
2016-05-17 20:07:45 +0300

    ....

--- smell-rpi/program.py
+++ smell-rpi/program.py
@@ -5,11 +5,13 @@ from rfreader.rfreader import RfReaderManager
 from manager import Manager
 from migutils.LoggerWrapper import LoggerWrapper
 import threading
+from server_comm import ServerComm
+import time 
 
 app = Flask(__name__)
 
 airPumpRunning = False
-gpioPin = 17
+
 hwManager = HardwareManager()
 #register to events from rfreader
 
@@ -28,6 +30,17 @@ def startSeq():
 #    #stop air pump TODO
 #     x = 0
 
+def initStartCheckLoop():
+    while True:
+        logger.info('initStartCheckLoop()')
+        json_data = ServerComm.startCheck() 
+        if json_data['status'] >= 0:     
+            output = json_data['start']
+            if int(output) == 1:
+                 hwManager.startSimSequence1()
+                 time.sleep(5)
+        time.sleep(5)
+
 def initRfLoop():
     manager = Manager()
     rfreader = RfReaderManager()
@@ -41,12 +54,15 @@ if __name__ == '__main__':
       #init logger
     logger = LoggerWrapper()
     logger.add_handler()
-    initRfLoop()
+   # initRfLoop()
 
-    t = threading.Thread(target=initRfLoop)
-    t.start()
-    #LedManager.colortrail()
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    t1 = threading.Thread(target=initRfLoop)
+    t1.start()
+
+    t2 = threading.Thread(target=initStartCheckLoop)
+    t2.start()
+    #LedManager().blink1_3()
+    #app.run(host='0.0.0.0', port=4000, debug=True)
 
 
 
